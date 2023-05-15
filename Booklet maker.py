881cd1efb992d0a570fe7df9d4fd2e23b0fd21a7
下田直樹 <naoki@iMac.local>
2023-05-15 14:43:16 +0900

    first commit

---
+++ Booklet maker.py
@@ -0,0 +1,40 @@
+# This program changes the page order of a given PDF file.
+# After operation, the sorted PDF file forms a BOOKLET when printed.
+
+import PyPDF2
+import glob2
+import sys
+sys.path.append('/opt/anaconda3/lib/python3.8/site-packages')
+
+
+def bind_PDF(src_path, dst_basepath):
+    # "src" stands for "SOURCE", "dst" for "DESTINATION"
+
+    src_pdf = PyPDF2.PdfFileReader(src_path)
+    secondary_pdf = PyPDF2.PdfFileWriter()
+    secondary_pdf.appendPagesFromReader(src_pdf)
+
+    # secondary_pdf is made by
+    # adding blank pages to the end of given PDF file
+    # so that it has "multiple of 4" pages.
+    # Precisely speaking, 4*[n/4] pages    ([] is CEILING function)
+    n = src_pdf.numPages
+    if(n % 4 != 0):
+        for i in range(4-n % 4):
+            secondary_pdf.addBlankPage()
+
+    dst_pdf = PyPDF2.PdfFileWriter()
+    N = secondary_pdf.getNumPages()
+    for i in range(N//4):
+        dst_pdf.addPage(secondary_pdf.getPage(-(2*i+1)))
+        dst_pdf.addPage(secondary_pdf.getPage(2*i))
+        dst_pdf.addPage(secondary_pdf.getPage(2*i+1))
+        dst_pdf.addPage(secondary_pdf.getPage(-(2*i+2)))
+
+    with open('{}.pdf'.format(dst_basepath), 'wb') as f:
+        dst_pdf.write(f)
+
+files = glob2.glob("./input/*")
+for filename in files:
+    # filename = "EXCFG21.pdf"  # Enter the file name HERE.
+    bind_PDF(filename, "output/"+filename[8:-4]+"_sorted")
---
+++ Merger.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append('/opt/anaconda3/lib/python3.8/site-packages')
+
+import PyPDF2
+from pathlib import Path
+
+# List of PDF files in the folder
+pdf_dir = Path("./pdf_files")
+pdf_files = sorted(pdf_dir.glob("*.pdf"))
+
+# Integrate all PDF files into one
+pdf_writer = PyPDF2.PdfFileWriter()
+for pdf_file in pdf_files:
+    pdf_reader = PyPDF2.PdfFileReader(str(pdf_file))
+    for i in range(pdf_reader.getNumPages()):
+        pdf_writer.addPage(pdf_reader.getPage(i))
+
+# Name of new file is consisted of 
+# names of the first and the last files
+merged_file = pdf_files[0].stem + "-" + pdf_files[-1].stem + ".pdf"
+
+# Save
+with open(merged_file, "wb") as f:
+    pdf_writer.write(f)
